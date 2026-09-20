779432d17db7f01793dec3e0f2640f47dc4b19e8
Wang <66399853+tw-repository@users.noreply.github.com>
2026-09-20 15:59:43 +0800

    Update hypergraph_utils.py

--- networks/hypergraph_utils.py
+++ networks/hypergraph_utils.py
@@ -1,9 +1,4 @@
-# --------------------------------------------------------
-# Utility functions for Hypergraph
-#
-# Author: Yifan Feng
-# Date: November 2018
-# --------------------------------------------------------
+
 import numpy as np
 
 
